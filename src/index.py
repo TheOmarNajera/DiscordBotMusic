166a3f4336edb6253ed750ca4b8b136d9614a728
Omar Najera <omar.navzz@gmail.com>
2021-09-18 17:52:06 -0500

    some changes

--- src/index.py
+++ src/index.py
@@ -2,16 +2,76 @@ import discord
 from discord.ext import commands
 from decouple import config
 import datetime
+from urllib import parse, request
+import re, os
+import youtube_dl
 
 token = str(config('bot_token'))
 
 bot = commands.Bot(command_prefix='>',description="This is a helper bot")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def play(ctx, url : str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
 
-    
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Tarea')
+    await voiceChannel.connect()
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+
+@bot.command()
+async def leave(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command()
+async def pause(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing.")
+
+
+@bot.command()
+async def resume(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("The audio is not paused.")
+
+
+@bot.command()
+async def stop(ctx):
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    voice.stop()
+
 @bot.command()
 async def info(ctx):
     embed = discord.Embed(title=f"{ctx.guild.name}", description="Lorem impsum asdasd", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
@@ -19,9 +79,19 @@ async def info(ctx):
     embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
     embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
     embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
-    print(f"{ctx.guild.icon}")
     embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
     await ctx.send(embed=embed)
+
+@bot.command()
+async def youtube(ctx, *, search):
+    query_string = parse.urlencode({'search_query': search})
+    html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
+    # print(html_content.read().decode())
+    search_results = re.findall(r"watch\?v=(\S{11})", html_content.read().decode())
+    print(search_results)
+    # I will put just the first result, you can loop the response to show more results
+    await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
+
 
 # Events
 @bot.event
