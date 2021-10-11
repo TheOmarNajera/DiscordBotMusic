474a7b168d04f695bc8b2389310e1ec55bd0a85e
Omar Najera <omar.navzz@gmail.com>
2021-10-11 10:44:19 -0500

    changes

--- src/index.py
+++ src/index.py
@@ -1,15 +1,213 @@
-import discord
-from discord.ext import commands
+""" import discord
+from discord.ext import commands """
 from decouple import config
 import datetime
+import asyncio
 from urllib import parse, request
 import re, os
-import youtube_dl
+""" 
+import youtube_dl 
+"""
 
 token = str(config('bot_token'))
 
-bot = commands.Bot(command_prefix='!',description="This is a helper bot")
+import discord
+from discord.ext import commands, tasks
+from discord.voice_client import VoiceClient
+import youtube_dl
 
+from random import choice
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+    @staticmethod
+    def youtube(search):
+        #print(search)
+        query_string = parse.urlencode({'search_query': search})
+        html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
+        search_results = re.findall(r"watch\?v=(\S{11})", html_content.read().decode())
+        url = 'https://www.youtube.com/watch?v=' + search_results[0]
+        return url
+
+client = commands.Bot(command_prefix='!')
+
+status = ['Jamming out to music!', 'Eating!', 'Sleeping!']
+queue = []
+
+@client.event
+async def on_ready():
+    change_status.start()
+    print('Bot is online!')
+
+@client.event
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name='general')
+    await channel.send(f'Welcome {member.mention}!  Ready to jam out? See `?help` command for details!')
+        
+@client.command(name='ping', help='This command returns the latency')
+async def ping(ctx):
+    await ctx.send(f'**Pong!** Latency: {round(client.latency * 1000)}ms')
+
+@client.command(name='hello', help='This command returns a random welcome message')
+async def hello(ctx):
+    responses = ['***grumble*** Why did you wake me up?', 'Top of the morning to you lad!', 'Hello, how are you?', 'Hi', '**Wasssuup!**']
+    await ctx.send(choice(responses))
+
+@client.command(name='die', help='This command returns a random last words')
+async def die(ctx):
+    responses = ['why have you brought my short life to an end', 'i could have done so much more', 'i have a family, kill them instead']
+    await ctx.send(choice(responses))
+
+@client.command(name='credits', help='This command returns the credits')
+async def credits(ctx):
+    await ctx.send('Made by `Omar Najera`')
+
+@client.command(name='creditz', help='This command returns the TRUE credits')
+async def creditz(ctx):
+    await ctx.send('**No one but me, lozer!**')
+
+@client.command(name='join', help='This command makes the bot join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("You are not connected to a voice channel")
+        return
+    
+    else:
+        channel = ctx.message.author.voice.channel
+
+    await channel.connect()
+
+@client.command(name='queue', help='This command adds a song to the queue')
+async def queue_(ctx, * ,url):
+    global queue
+
+    patron = r"(https:\/\/www\.youtube\.com\/watch\?v=(\S{11}))"
+    if re.match(patron,url):
+        pass
+    else:
+        url = YTDLSource.youtube(url)
+
+    queue.append(url)
+    await ctx.send(f'`{url}` added to queue!')
+
+@client.command(name='remove', help='This command removes an item from the list')
+async def remove(ctx, number):
+    global queue
+
+    try:
+        del(queue[int(number)])
+        await ctx.send(f'Your queue is now `{queue}!`')
+    
+    except:
+        await ctx.send('Your queue is either **empty** or the index is **out of range**')
+        
+@client.command(name='play', help='This command plays songs')
+async def play(ctx):
+    global queue
+
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    async with ctx.typing():
+        player = await YTDLSource.from_url(queue[0], loop=client.loop)
+        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+    await ctx.send('**Now playing:** {}'.format(player.title))
+    del(queue[0])
+
+@client.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.pause()
+
+@client.command(name='resume', help='This command resumes the song!')
+async def resume(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.resume()
+
+@client.command(name='view', help='This command shows the queue')
+async def view(ctx):
+    await ctx.send(f'Your queue is now `{queue}!`')
+
+@client.command(name='leave', help='This command stops makes the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    await voice_client.disconnect()
+
+@client.command(name='stop', help='This command stops the song!')
+async def stop(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.stop()
+
+@tasks.loop(seconds=20)
+async def change_status():
+    await client.change_presence(activity=discord.Game(choice(status)))
+
+@client.command()
+async def info(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Información sobre el Servidor", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
+    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
+    await ctx.send(embed=embed)
+
+# Events
+@client.event
+async def on_ready():
+    print('My Bot is Ready')
+
+client.run(token)
+
+""" 
 def youtube(search):
     print(search)
     query_string = parse.urlencode({'search_query': search})
@@ -94,9 +292,9 @@ async def stop(ctx):
         voice.stop()
         await ctx.send("Oh, stop!!")
     else:
-        await ctx.send("Currently no audio is playing.")
+        await ctx.send("Currently no audio is playing.") """
 
-@bot.command()
+""" @bot.command()
 async def info(ctx):
     embed = discord.Embed(title=f"{ctx.guild.name}", description="Información sobre el Servidor", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
     embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
@@ -110,5 +308,5 @@ async def info(ctx):
 @bot.event
 async def on_ready():
     print('My Bot is Ready')
-
-bot.run(token)
+ """
+""" bot.run(token) """
